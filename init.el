a94bd37db5395f15c427fb545c46b5157e8b797f
Adam C. Foltzer <acfoltzer@galois.com>
2017-12-21 10:43:42 -0800

    add some Rust packages

--- init.el
+++ init.el
@@ -70,6 +70,12 @@
   :ensure t
   :config (auctex-latexmk-setup))
 
+;;;_ , company
+(use-package company
+  :ensure t
+  :diminish company-mode
+  :commands company-mode)
+
 ;;;_ , cryptol-mode
 (use-package cryptol-mode :ensure t)
 
@@ -304,6 +310,19 @@
 (use-package rust-mode
   :ensure t
   :commands rust-mode)
+
+(use-package cargo
+  :ensure t)
+
+(use-package racer
+  :ensure t
+  :bind (("TAB" . company-indent-or-complete-common))
+  :init
+  (progn
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode)
+    (setq company-tooltip-align-annotations t)))
 
 ;;;_ , scheme
 (use-package scheme

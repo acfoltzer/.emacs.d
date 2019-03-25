d1a539ffad5ef7484e4ac0d6167befeeec17bbcd
Adam C. Foltzer <acfoltzer@fastly.com>
2019-03-25 11:50:42 -0700

    updates

--- init.el
+++ init.el
@@ -72,6 +72,13 @@
   :ensure t
   :config (auctex-latexmk-setup))
 
+;;;_ , c-mode
+(defun my-c-initialization-hook ()
+  (bind-key "C-c C-f" 'clang-format-buffer c-mode-map)
+  (c-toggle-comment-style -1))
+(add-hook 'c-initialization-hook 'my-c-initialization-hook)
+
+
 ;;;_ , company
 (use-package company
   :ensure t
@@ -384,7 +391,8 @@
              whitespace-mode)
   :init
   (progn
-    (add-hook 'prog-mode-hook 'whitespace-mode)
+    ;; (add-hook 'prog-mode-hook 'whitespace-mode)
+    (global-whitespace-mode -1)
     (setq whitespace-style
           '(face tabs trailing lines space-before-tab
             newline empty space-after-tab tab-mark))))
@@ -435,6 +443,12 @@
 ;; Automatically save desktop
 (desktop-save-mode 1)
 
+;; Wrap to column 100
+(setq-default fill-column 100)
+
+;; no more yes-or-no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;;;_. Local configuration
 
 ;; Anything specific to a machine should be in site-lisp/local-config
@@ -477,13 +491,43 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"))
+ '(beacon-color "#dc322f")
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(fci-rule-color "#073642")
+ '(frame-background-mode (quote dark))
+ '(magit-commit-arguments (quote ("--gpg-sign=2A91B421C62B535C")))
  '(package-selected-packages
    (quote
-    (yaml-mode virtualenvwrapper unicode-fonts color-theme-sanityinc-solarized racer cargo rust-mode purescript-mode meson-mode markdown-mode magit-todos magit helm-swoop helm-projectile helm-idris helm-ag helm-descbinds helm flycheck-haskell exec-path-from-shell elm-mode cryptol-mode company auctex-latexmk auctex diminish use-package))))
+    (dockerfile-mode cmake-mode vcl-mode clang-format flycheck-rust typescript-mode yaml-mode virtualenvwrapper unicode-fonts color-theme-sanityinc-solarized racer cargo rust-mode purescript-mode meson-mode markdown-mode magit-todos magit helm-swoop helm-projectile helm-idris helm-ag helm-descbinds helm flycheck-haskell exec-path-from-shell elm-mode cryptol-mode company auctex-latexmk auctex diminish use-package)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#cb4b16")
+     (60 . "#b58900")
+     (80 . "#859900")
+     (100 . "#2aa198")
+     (120 . "#268bd2")
+     (140 . "#d33682")
+     (160 . "#6c71c4")
+     (180 . "#dc322f")
+     (200 . "#cb4b16")
+     (220 . "#b58900")
+     (240 . "#859900")
+     (260 . "#2aa198")
+     (280 . "#268bd2")
+     (300 . "#d33682")
+     (320 . "#6c71c4")
+     (340 . "#dc322f")
+     (360 . "#cb4b16"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
